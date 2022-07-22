cfa662e0898a27172d795d9858e9db838d9d3aec
gamagegit <cgamage92@yahoo.com>
2022-07-22 12:13:28 -0700

    push all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import AllPets from "./components/AllPets";
+import AddPet from "./components/AddPet";
+import SinglePet from "./components/SinglePet";
+import EditPet from "./components/EditPet";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-5">
+      <h1>Pet Shelter</h1>
+      <Routes>
+        <Route exact path="/" element={<AllPets />}></Route>
+        <Route exact path="/pets/new" element={<AddPet />}></Route>
+        <Route exact path="/pets/:id" element={<SinglePet />}></Route>
+        <Route exact path="/pets/:id/edit" element={<EditPet />}></Route>
+      </Routes>
     </div>
   );
 }
